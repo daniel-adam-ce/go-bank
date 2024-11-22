c120726d577d298fdbbf9034e06ca6c4149e203a
Daniel Adam <danieladamce@gmail.com>
2024-11-21 21:06:11 -0800

    viper config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,28 +6,25 @@ import (
 	"os"
 	"testing"
 
-	"github.com/joho/godotenv"
+	"github.com/daniel-adam-ce/go-bank/util"
 	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const dbDriver = "postgres"
-
 func TestMain(m *testing.M) {
 	var err error
-	err = godotenv.Load("../../.env")
+
+	config, err := util.LoadConfig("../..")
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading config: ", err)
 	}
 
-	dbSource := os.Getenv("DB_SOURCE")
-	log.Print("test", dbSource)
 	// conn, err := pgxpool.New(context.Background(), dbSource)
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
