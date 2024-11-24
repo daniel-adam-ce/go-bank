07a44b9b78313b56f9c00cd687d75b2c46f22f9b
Daniel Adam <danieladamce@gmail.com>
2024-11-24 14:13:17 -0800

    user login route

--- api/server.go
+++ api/server.go
@@ -20,6 +20,7 @@ type Server struct {
 
 func NewServer(config util.Config, store db.Store) (*Server, error) {
 
+	// this can be interchanged with NewJWTMaker
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("ccanot create token maker: %v", err)
@@ -29,15 +30,27 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 		store:      store,
 		tokenMaker: tokenMaker,
 	}
-	router := gin.Default()
+
+	server.setupRouter()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	// users routes
+	return server, nil
+}
 
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func (server *Server) setupRouter() {
+
+	router := gin.Default()
+	// users routes
 	router.POST("/users", server.createUser)
+	// this is not RESTful
+	router.POST("/users/login", server.loginUser)
 
 	// accounts routes
 	router.POST("/accounts", server.createAccount)
@@ -48,12 +61,6 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-
-	return server, nil
-}
-
-func (server *Server) Start(address string) error {
-	return server.router.Run(address)
 }
 
 func errorResponse(err error) gin.H {
