fc82232199fe4467ec5dded2b1cff43f9162897e
Daniel Adam <danieladamce@gmail.com>
2024-12-02 23:58:20 -0800

    create and login rpc

--- api/user.go
+++ api/user.go
@@ -48,6 +48,7 @@ func (server *Server) createUser(ctx *gin.Context) {
 
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse((err)))
+		return
 	}
 
 	arg := db.CreateUserParams{
