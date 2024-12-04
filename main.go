4fad81306c23254a8e18f96f5218a747db105a61
Daniel Adam <danieladamce@gmail.com>
2024-12-03 23:23:19 -0800

    runGatewayServer

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/daniel-adam-ce/go-bank/api"
 	db "github.com/daniel-adam-ce/go-bank/db/sqlc"
 	"github.com/daniel-adam-ce/go-bank/gapi"
 	"github.com/daniel-adam-ce/go-bank/pb"
 	"github.com/daniel-adam-ce/go-bank/util"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -33,6 +37,8 @@ func main() {
 		log.Fatal("cannot connect to db: ", err)
 	}
 	store := db.NewStore(conn)
+
+	go runGatewayServer(config, store)
 	// runGinServer(config, store)
 
 	runGrpcServer(config, store)
@@ -69,5 +75,44 @@ func runGrpcServer(config util.Config, store db.Store) {
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Fatal("error starting grpc server: ", err)
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create grpc server: ", err)
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterGoBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.APIUrl)
+	if err != nil {
+		log.Fatal("error creating listener: ", err)
+	}
+
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("error starting HTTP gateway server: ", err)
 	}
 }
