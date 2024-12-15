9e17a09499aa2ffe39b3b804b6140e2ace637d60
Daniel Adam <danieladamce@gmail.com>
2024-12-14 21:06:32 -0800

    added verify email tx to rpc function

--- gapi/rpc_verify_email.go
+++ gapi/rpc_verify_email.go
@@ -3,9 +3,12 @@ package gapi
 import (
 	"context"
 
+	db "github.com/daniel-adam-ce/go-bank/db/sqlc"
 	"github.com/daniel-adam-ce/go-bank/pb"
 	"github.com/daniel-adam-ce/go-bank/val"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (server *Server) VerifyEmail(ctx context.Context, req *pb.VerifyEmailRequest) (*pb.VerifyEmailResponse, error) {
@@ -14,7 +17,18 @@ func (server *Server) VerifyEmail(ctx context.Context, req *pb.VerifyEmailReques
 		return nil, invalidArgumentError(violations)
 	}
 
-	res := &pb.VerifyEmailResponse{}
+	txResult, err := server.store.VerifyEmailTx(ctx, db.VerifyEmailTxParams{
+		EmailId:    req.GetEmailId(),
+		SecretCode: req.GetSecretCode(),
+	})
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to verify email")
+	}
+
+	res := &pb.VerifyEmailResponse{
+		IsVerified: txResult.User.IsEmailVerified,
+	}
 	return res, nil
 }
 
